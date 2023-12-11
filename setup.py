1fa4a01beea74dd7464fa96762c725c4ab4460fa
Harris Tzovanakis <me@drjova.com>
2023-12-11 14:38:55 +0100

    setup: fix versioning

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     install_requires=install_requires,
     tests_require=tests_require,
     extras_require=extras_require,
-    version="63.2.18",
+    version="63.2.19",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
